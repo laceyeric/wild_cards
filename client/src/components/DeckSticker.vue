1afbd7b8756efa1ac78ec262d7874ca76c0fa128
Jim Greaves <greavesjim3@gmail.com>
2020-02-20 14:14:29 -0700

    deck sticker changes

--- client/src/components/DeckSticker.vue
+++ client/src/components/DeckSticker.vue
@@ -2,9 +2,11 @@
   <div class="deck-sticker">
     <router-link :to="{name: 'deckDetails', params: { id: deckData._id} }">
       <div class="card" style="width: 20vh; height: 28vh;">
-        <div class="card-body">
-          <h5 class="card-title text-wrap">{{ deckData.title }}</h5>
-          <p class="card-text text-left text-wrap">{{ deckData.description }}</p>
+        <div class="inner" style="width: 93%; height: 95%;  display: block">
+          <div class="card-body">
+            <h3 class="card-title text-wrap">{{ deckData.title }}</h3>
+            <p class="card-text text-left text-wrap">{{ deckData.description }}</p>
+          </div>
         </div>
       </div>
     </router-link>
@@ -26,6 +28,15 @@ export default {
 <style scoped>
 .card {
   color: #2c3e50;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+}
+.inner {
+  display: flex;
+  border-radius: 0.4em;
+  border: solid 2px;
+  background-color: white;
 }
 a {
   text-decoration: none;
